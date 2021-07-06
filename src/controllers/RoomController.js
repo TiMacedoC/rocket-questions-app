ceb4768a2a0218db8b55aac685e0863a6b35cbb4
TiMacedoC <tiagomacedo99@hotmail.com>
2021-07-06 16:40:29 -0300

    criado banco de dados

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,7 +1,30 @@
+const Database = require("../db/config");
+
 module.exports = {
-    create(req, res) {
-        let roomId = "123456"
+    async create(req, res) {
+        const db = await Database()
+        const pass = req.body.password;
+
+        let roomId = ""
+
+        for (let i = 0; i < 6; i++) {
+            roomId += Math.floor(Math.random() * 10)
+        }
+
+        roomId = parseInt(roomId);
+
+
+        await db.run(`
+            INSERT INTO rooms(
+                id,
+                pass
+            ) VALUES (
+                ${roomId},
+                ${pass}
+            )
+        `)
 
         res.redirect(`/room/${roomId}`)
     }
 }
+
